af18106d3b5e361b279705a1a25f6e6a3fb04551
huge-pancake <huge.pancake.zxr@gmail.com>
2023-06-10 10:01:35 +0800

    chore: clear bins

--- .bin/init-gtk4-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export THEME_DIR="/usr/share/themes/Catppuccin-Mocha-Standard-Blue-Dark"
-mkdir -p "${HOME}/.config/gtk-4.0"
-ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
-ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
-ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
-unset THEME_DIR
