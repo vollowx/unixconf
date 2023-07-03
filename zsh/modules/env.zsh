6b01225f36d1a82c0119c177ee495f1e4e8f03a8
huge-pancake <huge.pancake.zxr@gmail.com>
2023-07-03 08:46:44 +0800

    feat!: refactor with `nullxception/dotfiles`

---
+++ zsh/modules/env.zsh
@@ -0,0 +1,17 @@
+# Path
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
+export XDG_SCREENSHOTS_DIR="$HOME/Pictures/Screenshots"
+
+# Others
+export EDITOR="nvim"
+export BROWSER="firefox-nightly"
+export TERMINAL="foot"
+export LANG="en_US.UTF-8"
+
+export BAT_THEME="Catppuccin-mocha"
+export EXA_ICON_SPACING=2
+export NEOVIDE_VSYNC=false
+export NEOVIDE_MULTIGRID=true
