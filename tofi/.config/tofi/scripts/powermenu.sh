765e3463a770e70d90bd48be885334d6f846d7d8
vollowx <muffinslog@proton.me>
2023-10-05 21:14:57 +0800

    feat(tofi): add powermenu

---
+++ tofi/.config/tofi/scripts/powermenu.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+actions="Lock\nPoweroff\nReboot\nSuspend\nHibernate\nLogout"
+
+selected=$(echo -e "$actions" | tofi --prompt-text ":power ")
+
+if [ ! -z "$selected" ]; then
+  case $selected in
+  "Lock")
+    swaylock -f
+    ;;
+  "Poweroff")
+    systemctl poweroff
+    ;;
+  "Reboot")
+    systemctl reboot
+    ;;
+  "Suspend")
+    systemctl suspend
+    ;;
+  "Hibernate")
+    systemctl hibernate
+    ;;
+  "Logout")
+    swaymsg exit
+    ;;
+  esac
+fi
