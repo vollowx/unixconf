ea7f110a7267cdb06c697cc166743621ca54aaba
vollowx <vollowx@proton.me>
2023-12-30 12:37:08 +0800

    fix(qutebrowser): allow js to modify clipboard
    fix(gtk): follow changed font

--- shared/.config/qutebrowser/config.py
+++ shared/.config/qutebrowser/config.py
@@ -4,5 +4,6 @@ catppuccin.setup(c, 'mocha', True)
 c.auto_save.session = True
 c.tabs.position = 'left'
 c.colors.webpage.preferred_color_scheme = 'dark'
+c.content.javascript.clipboard = 'access-paste'
 
 config.load_autoconfig()
