32440f1812d2f341d760efa145f2878a19233095
vollowx <vollowx@proton.me>
2024-01-20 23:03:37 +0800

    feat(colors): migrate to catppuccin-latte

--- init-papirus.sh
+++
@@ -1,1 +0,0 @@
-papirus-folders --color cat-mocha-lavender
--- shared/.config/qutebrowser/config.py
+++ shared/.config/qutebrowser/config.py
@@ -1,9 +1,5 @@
 import catppuccin
-catppuccin.setup(c, 'latte', True)
 
-c.auto_save.session = True
-c.tabs.position = 'top'
-c.colors.webpage.preferred_color_scheme = 'light'
-c.content.javascript.clipboard = 'access-paste'
+catppuccin.setup(c, "mocha", True)
 
 config.load_autoconfig()
