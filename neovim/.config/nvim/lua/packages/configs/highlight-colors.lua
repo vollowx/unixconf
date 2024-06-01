99324341303e0b88688fa1a65e06392b46a267b9
vollowx <vollowx@proton.me>
2024-06-02 03:29:25 +0800

    fix(neovim): `nvim-highlight-colors` ignore `lazy.nvim`

--- neovim/.config/nvim/lua/packages/configs/highlight-colors.lua
+++ neovim/.config/nvim/lua/packages/configs/highlight-colors.lua
@@ -1,3 +1,6 @@
-require('nvim-highlight-colors').setup({ render = 'virtual' })
+require('nvim-highlight-colors').setup({
+  render = 'virtual',
+  exclude_buftypes = { 'lazy' },
+})
 
 -- Test: #ffffff, rgb(255, 255, 0)
